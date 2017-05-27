d6feb9011b2813e3f7315aabfcabdba8b186a35d
Artur Puzio <cytadela8@interia.pl>
2017-05-27 11:54:35 +0200

    Added documentation, small changes

--- zabojca/check_names.sh
+++ zabojca/check_names.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# Skrypt do wyszukiwania błędnych (trole) i należących do gimnazjum (gimbusy) spośród zgłoszonych na Cenzora. Nie
+# weryfikuje klasy. (Można dodać porównując CLASS z FINGER_KLASA) Pokazuje osoby o takim samym nazwisku, gdy
+# uwzględniając imię nie można znaleźć.
+# Przyjmuje plik CSV z imieniem, nazwiskiem i klasą.
+#
+# USTAWIĆ:
+# - pod NAME_COL numer kolumny z imieniem i nazwiskiem
+# - pod CLASS_COL numer kolumny z klasą
+
+NAME_COL=2
+CLASS_COL=3
+
+if [ "$#" -ne "2" ]; then
+    echo Usage: $0 input.csv
+fi
+
 POPRAWNE=0
 LICZ=0
 while read l; do
   LICZ=$(($LICZ + 1))
-  NAME=$(echo $l | cut -d',' -f 2)
+  NAME=$(echo $l | cut -d',' -f $NAME_COL)
   NAME=$(echo $NAME | xargs)
-  CLASS=$(echo $l | cut -d',' -f 3)
-  #echo $NAME, klasa $CLASS
+  CLASS=$(echo $l | cut -d',' -f $CLASS_COL)
   NAME_S=$(echo $NAME | cut -d' ' -f 2)
   FINGER_RAW="$(finger $NAME_S 2>/dev/null)"
   if [ -z "$FINGER_RAW" ]; then
@@ -33,7 +48,7 @@ while read l; do
   fi
   FINGER_KLASA=$(echo $FINGER_DATA | grep "Directory:" | cut -f 4 -d ":" | cut -d'/' -f 3)
   if [[ $FINGER_KLASA == "gim" ]]; then
-    echo "$NAME - GIMBUS GIMBUS GIMBUS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1"
+    echo "$NAME - GIMBUS GIMBUS GIMBUS!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!1!"
   fi
 
   POPRAWNE=$(($POPRAWNE + 1))
